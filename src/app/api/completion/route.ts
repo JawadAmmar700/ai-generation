634fb284ec2bc7d56e4bcd809a8da549597a998b
JawadAmmar700 <JawadAmmarCode700@gmail.com>
2023-03-02 17:07:36 +0200

    first commit

---
+++ src/app/api/completion/route.ts
@@ -0,0 +1,62 @@
+export const runtime = "experimental-edge";
+
+export async function POST(request: Request) {
+  const { prompt } = await request.json();
+
+  const openaiOptions = {
+    model: "text-davinci-003",
+    prompt: prompt,
+    temperature: 0.7,
+    max_tokens: 2048,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    stream: true,
+    presence_penalty: 0.0,
+    n: 1,
+  };
+
+  const response = await fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    },
+    body: JSON.stringify(openaiOptions),
+  });
+
+  const reader = response.body?.getReader();
+
+  const decoder = new TextDecoder();
+  const encoder = new TextEncoder();
+
+  const stream = new ReadableStream({
+    start(controller) {
+      function push() {
+        reader?.read().then(({ done, value }) => {
+          try {
+            if (done) {
+              controller.close();
+              return;
+            }
+            const decoded = decoder.decode(value).split("data:")[1];
+            const chuckData = JSON.parse(decoded).choices[0].text;
+            const encode = encoder.encode(chuckData);
+            controller.enqueue(encode);
+            push();
+          } catch (e) {
+            controller.close();
+            return;
+          }
+        });
+      }
+
+      push();
+    },
+  });
+
+  return new Response(stream);
+}
+
+// export const config = {
+//   runtime: "edge",
+// };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,172 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+"use client";
+import { categoryTypes } from "@/lib/constants";
+import { Inter } from "next/font/google";
+import { useState } from "react";
+import Spinner from "@/components/snipper";
 
-const inter = Inter({ subsets: ['latin'] })
+export const runtime = "experimental-edge";
+
+const inter = Inter({ subsets: ["latin"] });
+
+type Choice = {
+  title: string;
+  description: string;
+};
 
 export default function Home() {
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+  const [selectedType, setSelectedType] = useState<string>("tv show or movie");
+  const [description, setDescription] = useState<string>("");
+  const [choices, setChoices] = useState<Choice[]>([]);
+  const [generating, setGenerating] = useState<boolean>(false);
+
+  const handleCategoryChange = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    category: string
+  ) => {
+    if (e.target.checked) {
+      setSelectedCategories([...selectedCategories, category]);
+    } else {
+      setSelectedCategories(selectedCategories.filter((c) => c !== category));
+    }
+  };
+
+  const generatePreferences = async () => {
+    const searchTerm = `Give me a list of 5 ${selectedType} recommendations ${
+      selectedCategories
+        ? `that fit all of the following categories: ${selectedCategories}`
+        : ""
+    }. ${
+      description
+        ? `Make sure it fits the following description as well: ${description}.`
+        : ""
+    } ${
+      selectedCategories || description
+        ? `If you do not have 5 recommendations that fit these criteria perfectly, do your best to suggest other ${selectedType}'s that I might like.`
+        : ""
+    } Please return this response as a numbered list with the ${selectedType}'s title, followed by a colon, and then a brief description of the ${selectedType}. There should be a line of whitespace between each item in the list.`;
+
+    setGenerating(true);
+
+    const response = await fetch("http://localhost:3000/api/completion", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ prompt: searchTerm }),
+    });
+    if (response.ok) {
+      const data = response.body;
+      if (!data) {
+        return;
+      }
+      const reader = data.getReader();
+      const decoder = new TextDecoder();
+      setGenerating(false);
+      let text = "";
+      while (true) {
+        const { value, done } = await reader.read();
+        const chunk = decoder.decode(value);
+        text += chunk;
+
+        const d = text
+          .split("\n")
+          .map((line) => line.trim())
+          .filter((line) => line.length > 0)
+          .map((line) => {
+            const [title, ...description] = line.split(":");
+            return {
+              title,
+              description: description.join(":"),
+            };
+          });
+        console.log(d);
+        setChoices(d);
+
+        if (done) {
+          break;
+        }
+      }
+    }
+  };
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main
+      className={`${inter.className} w-full flex flex-col space-y-5  items-center p-10 text-black`}
+    >
+      <h1 className="text-4xl font-bold text-center">
+        Get curated show and movie <br /> recommendation with Open Ai
+      </h1>
+      <div className="form-control w-full max-w-3xl">
+        <label className="label">
+          <span className="label-text text-black font-bold">
+            What kind of cinema you are searching for?
+          </span>
+        </label>
+        <select
+          className="select select-bordered bg-white"
+          value={selectedType}
+          onChange={(e) => setSelectedType(e.target.value)}
+        >
+          <option value="tv show or movie">No Preference</option>
+          <option value="tv show"> TV Show </option>
+          <option value="movie"> Movie </option>
+        </select>
+      </div>
+      <div className="flex flex-col space-y-2 w-full max-w-3xl items-start">
+        <h1 className="label-text text-black font-bold">
+          Select all categories that you like including.
+        </h1>
+        <div className="flex flex-wrap">
+          {categoryTypes.map((category, idx) => (
+            <label key={idx} className="label cursor-pointer">
+              <input
+                type="checkbox"
+                className="checkbox checkbox-sm mr-2"
+                onChange={(e) => handleCategoryChange(e, category)}
+                name={category}
+              />
+              <span className="label-text text-black font-medium">
+                {category}
+              </span>
+            </label>
+          ))}
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+      <div className="form-control w-full max-w-3xl">
+        <label className="label">
+          <span className="label-text font-bold text-black">
+            Write any other specification here. Be as picky as you'd like.
+          </span>
+        </label>
+        <textarea
+          className="textarea textarea-bordered h-24 bg-white"
+          placeholder="write a description here"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        ></textarea>
+      </div>
+      <button
+        onClick={generatePreferences}
+        className="max-w-3xl w-full p-3 bg-green-500 rounded ring-offset-2  ring-green-500/50 font-bold hover:ring-4"
+      >
+        Generate your Preferences
+      </button>
+      {generating ? (
+        <Spinner />
+      ) : (
+        <div className="w-full max-w-3xl flex flex-col space-y-5">
+          {choices.map((choice, idx) => (
+            <div
+              key={idx}
+              className="bg-white p-5 flex flex-col space-y-2 rounded-lg"
+            >
+              <h1 className="text-lg font-bold">{choice.title}</h1>
+              <p className="text-sm">{choice.description}</p>
+            </div>
+          ))}
         </div>
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      )}
     </main>
-  )
+  );
 }
